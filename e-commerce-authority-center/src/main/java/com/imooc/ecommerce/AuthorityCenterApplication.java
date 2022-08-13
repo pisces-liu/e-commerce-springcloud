73b35dd1fbd5aee2a17f1a4544eca7d6b0736e02
piscesliu <piscesliu66@qq.com>
2022-08-13 17:13:29 +0800

    6-3 测试连接数据库，测试 JPA

--- e-commerce-authority-center/src/main/java/com/imooc/ecommerce/AuthorityCenterApplication.java
+++ e-commerce-authority-center/src/main/java/com/imooc/ecommerce/AuthorityCenterApplication.java
@@ -3,12 +3,14 @@ package com.imooc.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * <h1>授权中心启动入口</h1>
  */
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableJpaAuditing  // 允许 Jpa 自动审计
 public class AuthorityCenterApplication {
 
     public static void main(String[] args) {
